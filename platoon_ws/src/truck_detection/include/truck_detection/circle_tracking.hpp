4ff8c6fd0c68839718d4958a7545265145f9e0ad
Jun-Beom Jung <80508780+JunSpring@users.noreply.github.com>
2025-06-11 12:33:22 +0200

    Add emergency stop logic to circle tracking

--- platoon_ws/src/truck_detection/include/truck_detection/circle_tracking.hpp
+++ platoon_ws/src/truck_detection/include/truck_detection/circle_tracking.hpp
@@ -3,6 +3,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/pose.hpp>
+#include <std_msgs/msg/bool.hpp>
 
 #include "obstacle_detector/msg/obstacles.hpp"
 #include "obstacle_detector/msg/circle_obstacle.hpp"
@@ -20,9 +21,11 @@ private:
   // ROS I/O
   rclcpp::Subscription<obstacle_detector::msg::Obstacles>::SharedPtr sub_;
   rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr pub_;
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr emergency_pub_;
 
   // Parameters
   int truck_id_;
+  double stop_distance_;
   
   void obstaclesCallback(const obstacle_detector::msg::Obstacles::ConstSharedPtr & msg);
   void updateParameters();
